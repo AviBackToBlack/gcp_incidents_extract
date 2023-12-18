470213e0fdd429b8ffcd15be07d0330af37847fb
AviBackToBlack <54722547+AviBackToBlack@users.noreply.github.com>
2023-12-18 16:47:15 +0000

    Add script

---
+++ gcp_indicents_extract.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
+# Constants
+# Replace 'your_project_id' with your actual project ID from Google Cloud Platform.
+# You can find your project ID in the Google Cloud Console.
+PROJECT_ID = 'your_project_id'
+
+# Replace 'your_authorization', 'your_key', 'your_apisid', 'your_hsid', 'your_sapisid', 'your_sid' and 'your_ssid'
+# with information obtained from requests to Google Cloud Console.
+# To retrieve this information:
+# 1. Log in to the Google Cloud Console.
+# 2. Navigate to the Monitoring section and open the network tab in your browser's development tools.
+# 3. Trigger an incident retrieval in the console and look for the outgoing API request.
+# 4. Inspect the request headers and copy the value of the parameters mentioned.
+# Note: This parameters are sensitive and should be kept secure.
+AUTHORIZATION = 'your_authorization'
+KEY = 'your_key'
+APISID = 'your_apisid'
+HSID = 'your_hsid'
+SAPISID = 'your_sapisid'
+SID = 'your_sid'
+SSID = 'your_ssid'
+
+# Replace 'YYYY-MM-DD' with the time range you are looking for
+FILTER = "(open_time%3E=%22YYYY-MM-DDT00:00:00Z%22%20AND%20open_time%3C=%22YYYY-MM-DDT23:59:59Z%22)"
+
+BASE_URL = f'https://monitoring.clients6.google.com/v3/projects/{PROJECT_ID}/incidents'
+PAGE_SIZE = 200
+
+def get_incidents(next_page_token=None):
+    cookies = {
+        'SID': SID,
+        'HSID': HSID,
+        'SSID': SSID,
+        'APISID': APISID,
+        'SAPISID': SAPISID,
+    }
+
+    headers = {
+        'Authorization': AUTHORIZATION,
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Origin': 'https://console.cloud.google.com',
+    }
+
+    url = f"{BASE_URL}?filter=%{FILTER}&pageSize={PAGE_SIZE}&key={KEY}"
+    if next_page_token:
+        url += f"&pageToken={next_page_token}"
+    response = requests.get(url, headers=headers, cookies=cookies)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+def main():
+    incidents = []
+    next_page_token = None
+
+    while True:
+        try:
+            data = get_incidents(next_page_token)
+            next_page_token = data.get('nextPageToken')
+            incidents.extend(data['incidents'])
+
+            if not next_page_token:
+                break
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+
+    with open(f'{PROJECT_ID}_incidents.json', 'w') as file:
+        json.dump(incidents, file, indent=4)
+
+if __name__ == "__main__":
+    main()
